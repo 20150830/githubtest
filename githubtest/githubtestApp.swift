23230beb9c3979bbe7b3171758a2fb429f48f80c
little <mail2little1@gmail.com>
2023-12-26 12:23:40 +0800

    Initial Commit

---
+++ githubtest/githubtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubtestApp.swift
+//  githubtest
+//
+//  Created by scren on 2023/12/26.
+//
+
+import SwiftUI
+
+@main
+struct githubtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
